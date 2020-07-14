c051f3a4c841818e843f6a153252571f9b093f89
Daniel Schnetler <daniel.schnetler@gmail.com>
2020-07-14 21:46:34 +0200

    Updated index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 
-asyn function() {
+async function() {
 	try {
 		const issueTitle = core.getInput("issue-title");
 		const jokeBody = core.getInput("joke");
